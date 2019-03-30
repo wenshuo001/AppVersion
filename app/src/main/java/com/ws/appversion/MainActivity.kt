659879362e90d09e2143a090c6811ba810232f83
wenshuo <1443678932@qq.com>
2019-03-30 17:35:37 +0800

    沉迷学习,日渐消瘦

--- app/src/main/java/com/ws/appversion/MainActivity.kt
+++ app/src/main/java/com/ws/appversion/MainActivity.kt
@@ -2,11 +2,59 @@ package com.ws.appversion
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
+import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.FrameLayout
+import android.support.design.widget.AppBarLayout
+import android.support.v4.app.Fragment
+import android.support.design.widget.TabLayout
+import android.support.v4.app.FragmentPagerAdapter
+import com.ws.appversion.frgment.MyFragment
+
+
+
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initViewPager()
+        setAppBarListener()
+    }
+
+    fun initViewPager(){
+        app_bar.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { appBarLayout, verticalOffset ->
+            val minScrollHeight = header.getMeasuredHeight()
+            var margin = minScrollHeight + verticalOffset
+            margin = if (margin > 0) 0 else margin
+            val layoutParams = sticky_view.getLayoutParams() as FrameLayout.LayoutParams
+            layoutParams.topMargin = margin
+            sticky_view.setLayoutParams(layoutParams)
+        })
+    }
+
+    val mFragmentList: MutableList<Fragment> = mutableListOf(MyFragment(), MyFragment(), MyFragment())
+
+    fun setAppBarListener(){
+        vp.setAdapter(object : FragmentPagerAdapter(supportFragmentManager) {
+            override fun getItem(position: Int): Fragment {
+                return mFragmentList[position]
+            }
+
+            override fun getCount(): Int {
+                return mFragmentList.size
+            }
+        })
+
+        table_layout.addTab(table_layout.newTab().setText("Tab1"))
+        table_layout.addTab(table_layout.newTab().setText("Tab2"))
+        table_layout.addTab(table_layout.newTab().setText("Tab3"))
+
+        table_layout.addOnTabSelectedListener(TabLayout.ViewPagerOnTabSelectedListener(vp))
+        vp.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(table_layout))
     }
 }
+
+
---
+++ app/src/main/java/com/ws/appversion/frgment/MyFragment.java
@@ -0,0 +1,73 @@
+package com.ws.appversion.frgment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.ws.appversion.R;
+
+
+/**
+ * Created by gavin
+ * Created date 17/8/3
+ */
+
+public class MyFragment extends Fragment {
+
+    RecyclerView mRv;
+    RecyclerView.Adapter mAdapter;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(
+                R.layout.fragment_list, container, false);
+        mRv = view.findViewById(R.id.rv);
+        initView();
+        return view;
+    }
+
+    private void initView() {
+        LinearLayoutManager manager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+        mRv.setLayoutManager(manager);
+        mAdapter = new RecyclerView.Adapter() {
+            @Override
+            public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_recycler_view, parent, false);
+                return new Holder(view);
+            }
+
+            @Override
+            public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int position) {
+                Holder holder = (Holder) viewHolder;
+                holder.mTextView.setText("current position: " + position);
+            }
+
+            @Override
+            public int getItemCount() {
+                return 30;
+            }
+        };
+        mRv.setAdapter(mAdapter);
+    }
+
+    static class Holder extends RecyclerView.ViewHolder {
+        TextView mTextView;
+        public Holder(View itemView) {
+            super(itemView);
+            mTextView = itemView.findViewById(R.id.tv);
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+
+    }
+}
